063b01091c3e8be7fe57766be9b4ae41fc6cef09
LinaChen <sskura0477@gmail.com>
2024-05-16 23:11:58 +0800

    add footer component

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,20 +1,38 @@
 <template>
-  <div class="container py-10">
-    <div class="mb-10">
-      <p class="text-center text-primary-700 mb-1">若有數位產品設計和開發相關問題</p>
-      <p class="text-center text-primary-700 mb-6">歡迎填寫表單，或直接與我聯絡！</p>
-      <p class="font-normal">service@hexschool.com</p>
+  <div class="container py-10 lg:py-20">
+    <div class="lg:flex lg:justify-between lg:items-center">
+      <div class="text-center mb-10 lg:text-left">
+        <img src="../assets/image/logo.svg" class="mx-auto mb-6 lg:mx-0" alt="noel" />
+        <p class="text-primary-700 mb-1">若有數位產品設計和開發相關問題</p>
+        <p class="text-primary-700 mb-6">歡迎填寫表單，或直接與我聯絡！</p>
+        <p class="font-normal">service@hexschool.com</p>
+      </div>
+      <ul class="list-unstyled">
+        <li class="mb-6">
+          <a href="#" class="flex items-center bg-primary-900 rounded-lg py-1 px-6">
+            <img src="../assets/image/list.svg" alt="list" class="me-2" />
+            <span class="text-primary-50">填寫表單</span>
+          </a>
+        </li>
+        <li class="mb-6">
+          <a href="#" class="flex items-center border border-primary-700 rounded-lg py-1 px-6">
+            <img src="../assets/image/instagram.svg" alt="hexschool_demo" class="me-2" />
+            <span class="text-primary-700">hexschool_demo</span>
+          </a>
+        </li>
+        <li>
+          <a href="#" class="flex items-center border border-primary-700 rounded-lg py-1 px-6">
+            <img src="../assets/image/facebook.svg" alt="hexschoolTW" class="me-2" />
+            <span class="text-primary-700">hexschoolTW</span>
+          </a>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li>
-        <a href="#"> </a>
-      </li>
-      <li>
-        <a href="#"> </a>
-      </li>
-      <li>
-        <a href="#"> </a>
-      </li>
-    </ul>
   </div>
 </template>
+
+<style>
+.list-unstyled {
+  list-style-type: none;
+}
+</style>
