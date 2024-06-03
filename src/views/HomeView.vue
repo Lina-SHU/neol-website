8f97f56569894bd2bcc881819fbbb41742dc390d
LinaChen <sskura0477@gmail.com>
2024-06-03 20:42:45 +0800

    add 作品 sections

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,96 @@
 <script setup></script>
 
 <template>
+  <!-- 作品介紹 -->
+  <h2 class="flex items-center justify-center py-10 lg:pt-40 lg:pb-20">
+    <img src="../assets/image/title-deco-left-sm.png" alt="" class="lg:hidden" />
+    <img src="../assets/image/title-deco-left-lg.png" alt="" class="hidden lg:block" />
+    <span class="text-primary-700 text-2xl font-bold mx-4 lg:text-4xl">作品介紹</span>
+    <img src="../assets/image/title-deco-right-sm.png" alt="" class="lg:hidden" />
+    <img src="../assets/image/title-deco-right-lg.png" alt="" class="hidden lg:block" />
+  </h2>
+  <!-- 星際旅行訂票平台 -->
+  <section class="bg-star lg:pt-20">
+    <div class="lg:container">
+      <div class="lg:flex lg:items-center lg:-mx-3">
+        <div class="bg-star-sm bg-center py-[90px] lg:mr-[49px] lg:py-0">
+          <div class="px-10 relative z-10">
+            <img src="../assets/image/homepage-work1.png" alt="" />
+          </div>
+        </div>
+        <div
+          class="px-3 pt-6 pb-12 lg:relative lg:z-10 lg:w-6/12 lg:bg-primary-50 lg:bg-opacity-10 lg:rounded-3xl lg:pt-20 lg:pb-14 lg:px-10 2xl:w-4/12"
+        >
+          <h3
+            class="font-bold text-center text-xl mb-2 lg:text-left lg:text-primary-50 lg:text-2xl"
+          >
+            星際旅行訂票平台
+          </h3>
+          <p class="text-primary-600 text-center mb-4 lg:text-left lg:text-primary-50 lg:mb-6">
+            悠遊宇宙的夢想，從這裡開始實現
+          </p>
+          <ul
+            class="flex justify-center mb-5 lg:pb-10 lg:border-b lg:border-primary-50 lg:justify-start lg:mb-10"
+          >
+            <li>
+              <div class="bg-primary-200 rounded-2xl py-1 px-3 mr-2 lg:mr-4">網頁設計</div>
+            </li>
+            <li>
+              <div class="bg-primary-200 rounded-2xl py-1 px-3 mr-2 lg:mr-4">響應式設計</div>
+            </li>
+            <li>
+              <div class="bg-primary-200 rounded-2xl py-1 px-3">Bootstrap</div>
+            </li>
+          </ul>
+          <button
+            class="bg-primary-900 text-primary-50 font-bold rounded-[5px] flex items-center px-4 py-2 mx-auto lg:text-xl lg:px-8 lg:py-4"
+          >
+            完整介紹
+            <img src="../assets/image/arrow.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- 理財APP -->
+  <section class="bg-app lg:py-[200px]">
+    <div class="lg:container">
+      <div class="lg:flex lg:items-center lg:-mx-3">
+        <div class="bg-app-sm bg-center"></div>
+        <div
+          class="px-3 pt-6 pb-20 lg:relative lg:z-10 lg:w-6/12 lg:bg-primary-50 lg:bg-opacity-10 lg:rounded-3xl lg:pt-20 lg:pb-14 lg:px-10 2xl:w-4/12"
+        >
+          <h3
+            class="font-bold text-center text-xl mb-2 lg:text-left lg:text-primary-50 lg:text-2xl"
+          >
+            理財APP
+          </h3>
+          <p class="text-primary-600 text-center mb-4 lg:text-left lg:text-primary-50 lg:mb-6">
+            連動帳戶與行動支付，讓 AI 提供您最好的理財建議
+          </p>
+          <ul
+            class="flex justify-center mb-5 lg:pb-10 lg:border-b lg:border-primary-50 lg:justify-start lg:mb-10"
+          >
+            <li>
+              <div class="bg-primary-200 rounded-2xl py-1 px-3 mr-2 lg:mr-4">APP設計</div>
+            </li>
+            <li>
+              <div class="bg-primary-200 rounded-2xl py-1 px-3 mr-2 lg:mr-4">APP設計</div>
+            </li>
+            <li>
+              <div class="bg-primary-200 rounded-2xl py-1 px-3">React</div>
+            </li>
+          </ul>
+          <button
+            class="bg-primary-900 text-primary-50 font-bold rounded-[5px] flex items-center px-4 py-2 mx-auto lg:text-xl lg:px-8 lg:py-4"
+          >
+            完整介紹
+            <img src="../assets/image/arrow.svg" alt="" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </section>
   <!-- 醫美診所官網 -->
   <section class="bg-clinic lg:pt-[176px] lg:pb-[112px]">
     <div class="lg:container">
@@ -21,7 +111,9 @@
           <p class="text-primary-600 text-center mb-4 lg:text-left lg:text-primary-50 lg:mb-6">
             永保青春的秘密，從粹究開始
           </p>
-          <ul class="flex justify-center mb-5 lg:pb-10 lg:border-b lg:border-primary-50 lg:mb-10">
+          <ul
+            class="flex justify-center mb-5 lg:pb-10 lg:border-b lg:border-primary-50 lg:justify-start lg:mb-10"
+          >
             <li>
               <div class="bg-primary-200 rounded-2xl py-1 px-3 mr-2 lg:mr-4">網頁設計</div>
             </li>
@@ -45,13 +137,13 @@
   <!-- 服務項目 -->
   <section class="bg-service pt-10 pb-20 lg:py-30">
     <div class="container">
-      <h3 class="flex items-center justify-center mb-10 lg:mb-20">
+      <h2 class="flex items-center justify-center mb-10 lg:mb-20">
         <img src="../assets/image/title-deco-left-sm.png" alt="" class="lg:hidden" />
         <img src="../assets/image/title-deco-left-lg.png" alt="" class="hidden lg:block" />
         <span class="text-primary-700 text-2xl font-bold mx-4 lg:text-4xl">服務項目</span>
         <img src="../assets/image/title-deco-right-sm.png" alt="" class="lg:hidden" />
         <img src="../assets/image/title-deco-right-lg.png" alt="" class="hidden lg:block" />
-      </h3>
+      </h2>
       <ul class="serviceList mb-10 lg:flex lg:flex-wrap lg:-mx-3 lg:mb-20">
         <li class="w-[82%] mx-auto mb-6 lg:w-3/12 lg:px-3 lg:mb-0">
           <div class="bg-service-item flex justify-center items-center rounded-2xl">
@@ -118,6 +210,45 @@
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+}
+
+.bg-star-sm {
+  background-image: url('../assets/image/homepage-work1-bg.png');
+  @media (min-width: 960px) {
+    background: none;
+  }
+}
+
+.bg-star {
+  @media (min-width: 960px) {
+    background-image: url('../assets/image/homepage-work1-bg.png');
+  }
+}
+
+.bg-app-sm {
+  background-image: url('../assets/image/homepage-work2-sm.png');
+  height: 390px;
+  @media (min-width: 960px) {
+    background: none;
+    height: auto;
+  }
+}
+
+.bg-app {
+  @media (min-width: 960px) {
+    position: relative;
+    background-image: url('../assets/image/homepage-work2-lg.png');
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 50%;
+      height: 100%;
+      background-color: rgba(13, 12, 7, 0.5);
+      z-index: 1;
+    }
+  }
 }
 
 .bg-clinic-sm {
