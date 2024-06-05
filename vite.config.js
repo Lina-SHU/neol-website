696ff629b975d4003e1569ea50a5172b4126d7c6
LinaChen <sskura0477@gmail.com>
2024-06-05 21:40:17 +0800

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/neol-website/',
+  base: process.env.NODE_ENV === 'production' ? '/neol-website/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
